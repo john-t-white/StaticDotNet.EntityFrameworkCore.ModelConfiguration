260db5ecef1790aaac19adc2f738f3601c37329a
John T. White <john.t.white@gmail.com>
2016-11-20 21:14:19 -0500

    Added Example project. Added extension methods to DbContextOptionsBuilder to add support for IEntityTypeConfigurations.

---
+++ src/StaticDotNet.EntityFrameworkCore.ModelConfiguration/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using StaticDotNet.EntityFrameworkCore.ModelConfiguration.Infrastructure;
+using StaticDotNet.EntityFrameworkCore.ModelConfiguration.Providers;
+using StaticDotNet.ParameterValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace StaticDotNet.EntityFrameworkCore.ModelConfiguration
+{
+	/// <summary>
+	/// Adds methods to <see cref="DbContextOptionsBuilder" /> to add support for <see cref="IEntityTypeConfiguration" />.
+	/// </summary>
+	public static class DbContextOptionsBuilderExtensions
+	{
+		/// <summary>
+		/// Adds <see cref="IEntityTypeConfiguration" /> from all of the <paramref name="assemblies" /> specified.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		/// <param name="assemblies">The assemblies to search for <see cref="IEntityTypeConfiguration>" />.</param>
+		/// <returns>The builder.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="assemblies" /> is null.</exception>
+		public static DbContextOptionsBuilder AddEntityTypeConfigurations( this DbContextOptionsBuilder builder, params Assembly[] assemblies )
+		{
+			Parameter.Validate( assemblies, nameof( assemblies ) )
+				.IsNotNull();
+
+			AssemblyEntityTypeConfigurationProvider provider = new AssemblyEntityTypeConfigurationProvider( assemblies );
+
+			return builder.AddEntityTypeConfigurations( provider );
+		}
+
+		/// <summary>
+		/// Adds <see cref="IEntityTypeConfiguration" /> base on the <paramref name="provider" />.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		/// <param name="provider">The <see cref="IEntityTypeConfigurationProvider" />.</param>
+		/// <returns>The builder.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="provider" /> is null.</exception>
+		public static DbContextOptionsBuilder AddEntityTypeConfigurations( this DbContextOptionsBuilder builder, IEntityTypeConfigurationProvider provider )
+		{
+			Parameter.Validate( builder, nameof( builder ) )
+				.IsNotNull();
+
+			Parameter.Validate( provider, nameof( provider ) )
+				.IsNotNull();
+
+			EntityTypeConfigurationOptionsExtension optionsExtension = new EntityTypeConfigurationOptionsExtension( provider );
+
+			( ( IDbContextOptionsBuilderInfrastructure )builder ).AddOrUpdateExtension( optionsExtension );
+
+			return builder;
+		}
+	}
+}
---
+++ tests/StaticDotNet.EntityFrameworkCore.ModelConfiguration.UnitTests/DbContextOptionsBuilderExtensions_AddEntityTypeConfigurations.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using StaticDotNet.EntityFrameworkCore.ModelConfiguration.Infrastructure;
+using StaticDotNet.EntityFrameworkCore.ModelConfiguration.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StaticDotNet.EntityFrameworkCore.ModelConfiguration.UnitTests
+{
+    public class DbContextOptionsBuilderExtensions_AddEntityTypeConfigurations
+    {
+		[Fact]
+		public void DbContextOptionsBuilderExtensions_AddEntityTypeConfigurations_WithAssembliesReturnsCorrectly()
+		{
+			Assembly[] assemblies = Array.Empty<Assembly>();
+
+			DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+
+			builder.AddEntityTypeConfigurations( assemblies );
+
+			EntityTypeConfigurationOptionsExtension optionsExtension = builder.Options.FindExtension<EntityTypeConfigurationOptionsExtension>();
+
+			Assert.NotNull( optionsExtension );
+
+			AssemblyEntityTypeConfigurationProvider provider = optionsExtension.Provider as AssemblyEntityTypeConfigurationProvider;
+			Assert.NotNull( provider );
+			Assert.Same( assemblies, provider.Assemblies );
+		}
+
+		[Fact]
+		public void DbContextOptionsBuilderExtensions_AddEntityTypeConfigurations_WithNullAssembliesThrowsArgumentNullException()
+		{
+			Assembly[] assemblies = null;
+
+			DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+
+			Assert.Throws<ArgumentNullException>( nameof( assemblies ), () => builder.AddEntityTypeConfigurations( assemblies ) );
+		}
+
+		[Fact]
+		public void DbContextOptionsBuilderExtensions_AddEntityTypeConfigurations_WithProviderReturnsCorrectly()
+		{
+			IEntityTypeConfigurationProvider provider = Substitute.For<IEntityTypeConfigurationProvider>();
+
+			DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+
+			builder.AddEntityTypeConfigurations( provider );
+
+			EntityTypeConfigurationOptionsExtension optionsExtension = builder.Options.FindExtension<EntityTypeConfigurationOptionsExtension>();
+
+			Assert.NotNull( optionsExtension );
+			Assert.Same( provider, optionsExtension.Provider );
+		}
+
+		[Fact]
+		public void DbContextOptionsBuilderExtensions_AddEntityTypeConfigurations_WithNullProviderThrowsArgumentNullException()
+		{
+			IEntityTypeConfigurationProvider provider = null;
+
+			DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+
+			Assert.Throws<ArgumentNullException>( nameof( provider ), () => builder.AddEntityTypeConfigurations( provider ) );
+		}
+	}
+}
